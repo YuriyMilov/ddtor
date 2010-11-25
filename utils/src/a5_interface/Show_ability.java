90a167c4dca8d4d08771b31ed09f74863cee8373
ymdata <ymdata@21e05f49-3ddf-bb0c-8055-30ea1052890c>
2010-11-25 15:33:31 +0000

    a5_interface (Ability) moved from the _Jigsaw servlet folder to zzz

---
+++ utils/src/a5_interface/Show_ability.java
@@ -0,0 +1,13 @@
+package a5_interface;
+
+public class Show_ability {
+public static void main(String[] args) {
+	Ability tab = null;
+	
+	tab = new Q1_with_ability();
+	tab.tell();
+	
+	tab = new Q2_with_ability();
+	tab.tell();
+}
+}
